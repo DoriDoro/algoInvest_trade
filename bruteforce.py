7a64058cf07a8665a02882419cf3c9a670a1b386
doridoro <dorothea.reher@gmail.com>
2023-06-16 18:12:22 +0200

    finalised generate_combinations

--- bruteforce.py
+++ bruteforce.py
@@ -1,34 +1,37 @@
 import csv
 
-# data_shares = []
-#
-# with open('data_shares.csv', 'r') as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         data_shares.append(row)
-#
-# # remove first list with header
-# data_shares = data_shares[1:]
-# # print('data', data_shares)
 
-data_shares = [
-    ["Action-1", 20, 0.25],
-    ["Action-2", 30, 3],
-    ["Action-3", 50, 7.5],
-    ["Action-4", 70, 14],
-    ["Action-5", 60, 10.2]
-]
+def load_data():
+    data_shares = []
+
+    with open('data_shares.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data_shares.append(row)
+
+    # remove first list with header
+    data_shares = data_shares[1:]
+
+    return data_shares
 
 
 def generate_combinations(shares, budget):
+    """ final result list combinations_with_max_profit looks like:
+    [[share[i], share[i+1], share[i+2], total_price, average_profit]
+    """
+
     combinations = []
     generate_combinations_recursive(shares, budget, [], combinations)
 
     for combi in combinations:
         profit = 0
+        total_price = 0
         for share in combi:
             profit += float(share[2])
-        average_profit = float(profit / len(combi))
+            total_price += int(share[1])
+
+        combi.append(total_price)
+        average_profit = round(float(profit / len(combi[:-1])), 2)
         combi.append(average_profit)
 
     combinations_with_max_profit = max(combinations, key=lambda x: x[-1])
@@ -55,9 +58,11 @@ def generate_combinations_recursive(shares, budget, current_combination, combina
             shares[1:], budget - int(share[1]), current_combination, combinations
         )
         current_combination.pop()
+
     combinations = generate_combinations_recursive(shares[1:], budget, current_combination, combinations)
     return combinations
 
 
-generate_combinations = generate_combinations(data_shares, budget=100)
+data_shares = load_data()
+generate_combinations = generate_combinations(data_shares, budget=500)
 print('end result', generate_combinations)
